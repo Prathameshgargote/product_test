308aeccae3779cb68222bd4de19ad2e5b81f849c
Prathameshgargote <patyagargote@gmail.com>
2025-04-26 12:39:32 +0530

    _chang1_Deploy

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -5,29 +5,22 @@ import { Iproduct } from 'src/app/model/product';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
   @Input() image!: string;
   @Input() category!: string;
   @Input() title!: string;
   @Input() price!: string;
-
-  proArr!:Array<Iproduct>
-  constructor( 
-    private _productService:ProductService
-  ) { }
+  hoverproduct: any = null;
+  proArr!: Array<Iproduct>;
+  constructor(private _productService: ProductService) {}
 
   ngOnInit(): void {
-
     // this.proArr=this._productService.fetchallproduct()
-    this._productService.getproduct()
-    .subscribe(res=>{
+    this._productService.getproduct().subscribe((res) => {
       console.log(res);
-      this.proArr=res
-    })
-
-
+      this.proArr = res;
+    });
   }
-
 }
